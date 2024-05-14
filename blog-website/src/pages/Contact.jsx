80900158cfb616c11e7e327c84df2ec76c5365b3
Joseph Awe <130408142+Jossyboydgenius@users.noreply.github.com>
2024-05-14 18:45:47 +0000

    style: Update Contact page with new header design

--- blog-website/src/pages/Contact.jsx
+++ blog-website/src/pages/Contact.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const Contact = () => {
   return (
-    <div>Contact</div>
+    <div>
+      <div className='py-40 bg-black text-center text-white px-4'>
+      <h2 className='text-5xl lg:text-7xl leading-snug font-bold mb-5'>Contact Us Page</h2>
+      </div>
+    </div>
   )
 }
 
