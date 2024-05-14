43c33f25573ae87137dc3a2a49c330abceef9061
Joseph Awe <130408142+Jossyboydgenius@users.noreply.github.com>
2024-05-14 17:41:02 +0000

    refactor: Update Navbar component to improve navigation links and menu toggle functionality

--- blog-website/src/components/Navbar.jsx
+++ blog-website/src/components/Navbar.jsx
@@ -13,10 +13,10 @@ const Navbar = () => {
   // NavItems
   const navItems = [
     { path: '/', link: 'Home' },
-    { path: '/services', link: 'services' },
-    { path: '/about', link: 'about' },
-    { path: '/blogs', link: 'blogs' },
-    { path: '/contact', link: 'contact' },
+    { path: '/services', link: 'Services' },
+    { path: '/about', link: 'About' },
+    { path: '/blogs', link: 'Blogs' },
+    { path: '/contact', link: 'Contact' },
   ]
   return (
     <header className='bg-black text-white fixed top-0 left-0 right-0'>
@@ -27,7 +27,13 @@ const Navbar = () => {
         <ul className='md:flex gap-12 text-lg hidden'>
           {
             navItems.map(({ path, link }) => <li className='text-white' key={path}>
-              <NavLink to={path}>{link}</NavLink>
+              <NavLink className={({ isActive, isPending }) =>
+                      isActive
+                        ? "active"
+                        : isPending
+                        ? "pending"
+                        : ""
+                    } to={path}>{link}</NavLink>
             </li>)
           }
         </ul>
@@ -44,11 +50,23 @@ const Navbar = () => {
         <div className='md:hidden'>
           <button onClick={toggleMenu} className='cursor-pointer'>
             {
-              isMenuOpen ? <FaXmark className='w-5 h-5'/> : <FaBars className='w-5 h-5'/>
+              isMenuOpen ? <FaXmark className='w-5 h-5' /> : <FaBars className='w-5 h-5' />
             }
-            </button>
+          </button>
         </div>
       </nav>
+      {/* Menu Items only for mobile*/}
+      <div>
+
+        <ul className={`md:hidden gap-12 text-lg block space-y-4 px-4 py-6 mt-14 bg-white 
+      ${isMenuOpen ? "fixed top-0 left-0 w-full transition-all ease-out duration-150" : "hidden"}`}>
+          {
+            navItems.map(({ path, link }) => <li className='text-black' key={path}>
+              <NavLink onClick={toggleMenu} to={path}>{link}</NavLink>
+            </li>)
+          }
+        </ul>
+      </div>
     </header>
   )
 }
