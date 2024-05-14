63af6dba6f5456fd9acd0abee9aa9952c056aebc
Joseph Awe <130408142+Jossyboydgenius@users.noreply.github.com>
2024-05-14 18:45:29 +0000

    feat: Update Blogs page with BlogPage component

--- blog-website/src/pages/Blogs.jsx
+++ blog-website/src/pages/Blogs.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import BlogPage from '../components/BlogPage'
 
 const Blogs = () => {
   return (
-    <div>Blogs</div>
+    <div>
+      <div className='py-40 bg-black text-center text-white px-4'>
+      <h2 className='text-5xl lg:text-7xl leading-snug font-bold mb-5'>Blog Page</h2>
+      </div>
+      {/* All blogs container */}
+      <div className='max-w-7xl mx-auto'>
+        <BlogPage />
+      </div>
+    </div>
   )
 }
 
