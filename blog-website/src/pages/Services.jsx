aa1efba5d344f431e248d2415ca535969b48240d
Joseph Awe <130408142+Jossyboydgenius@users.noreply.github.com>
2024-05-14 18:45:55 +0000

    feat: Update Services page with new header design

--- blog-website/src/pages/Services.jsx
+++ blog-website/src/pages/Services.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const Services = () => {
   return (
-    <div>Services</div>
+    <div>
+      <div className='py-40 bg-black text-center text-white px-4'>
+      <h2 className='text-5xl lg:text-7xl leading-snug font-bold mb-5'>Services Page</h2>
+      </div>
+    </div>
   )
 }
 
