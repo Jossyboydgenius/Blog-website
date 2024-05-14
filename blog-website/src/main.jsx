7234b5759363bed75a4f25a99289b2f3a38ba676
Joseph Awe <130408142+Jossyboydgenius@users.noreply.github.com>
2024-05-14 16:54:15 +0000

    feat: Add new pages and routes for Home, Blogs, About, Contact, and Services

--- blog-website/src/main.jsx
+++ blog-website/src/main.jsx
@@ -6,6 +6,11 @@ import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
+import Home from './pages/Home.jsx';
+import Blogs from './pages/Blogs.jsx';
+import About from './pages/About.jsx';
+import Contact from './pages/Contact.jsx';
+import Services from './pages/Services.jsx';
 
 
 const router = createBrowserRouter([
@@ -15,7 +20,24 @@ const router = createBrowserRouter([
     children: [
       {
       path: "/",
-      }
+      element: <Home/>
+      },
+      {
+        path: "/blogs",
+        element: <Blogs />
+      },
+      {
+        path: "/about",
+        element: <About />
+      },
+      {
+        path: "/contact",
+        element: <Contact />
+      },
+      {
+        path: "/services",
+        element: <Services />
+      },
     ]
   },
 ]);
