992178a09db467a119be05676771811b5c49f786
Joseph Awe <130408142+Jossyboydgenius@users.noreply.github.com>
2024-05-14 18:44:58 +0000

    style: Update About page with new header design

--- blog-website/src/pages/About.jsx
+++ blog-website/src/pages/About.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const About  = () => {
   return (
-    <div>About Page</div>
+    <div>
+      <div className='py-40 bg-black text-center text-white px-4'>
+      <h2 className='text-5xl lg:text-7xl leading-snug font-bold mb-5'>About Us Page</h2>
+      </div>
+    </div>
   )
 }
 
