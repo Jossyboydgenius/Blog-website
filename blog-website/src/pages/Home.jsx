84ac2993bf3b1eb14dc9d53fa63309d1b9e48323
Joseph Awe <130408142+Jossyboydgenius@users.noreply.github.com>
2024-05-15 01:56:12 +0000

    feat: Add lazy loading for images on Home page

--- blog-website/src/pages/Home.jsx
+++ blog-website/src/pages/Home.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import Banner from '../components/Banner'
+import BlogPage from '../components/BlogPage'
 
 const Home = () => {
   return (
     <div>
       <Banner />
+
+      <div className='max-w-7xl mx-auto'>
+        <BlogPage />
+      </div>
     </div>
   )
 }
