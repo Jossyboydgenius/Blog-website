21183fb7139a56d5b1f0ef39e7671f089d4204b9
Joseph Awe <130408142+Jossyboydgenius@users.noreply.github.com>
2024-05-15 01:53:44 +0000

    feat: Add Footer component to App for displaying a footer on all pages

--- blog-website/src/App.jsx
+++ blog-website/src/App.jsx
@@ -1,12 +1,14 @@
 import { Outlet } from 'react-router-dom'
 import './App.css'
 import Navbar from './components/Navbar'
+import Footer from './components/Footer'
 
 function App() {
   return (
     <>
       <Navbar/>
       <Outlet />
+      <Footer/>
     </>
   )
 }
